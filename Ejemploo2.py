2b9bb9b13ddbeec1dee1c66cb953cb65ed849018
alemirella <72184645@continental.edu.pe>
2024-09-09 15:20:58 -0500

    Modificacion ejercicio 02 y 03

--- Ejemploo2.py
+++ Ejemploo2.py
@@ -1,12 +1,18 @@
-def calcular(a, b, c):
-    res = a * b + c
-    return res
 
-def principal():
-    x = 5
-    y = 3
-    z = 7
-    resultado = calcular(x, y, z)
-    print("El resultado es:", resultado)
+def multiplicacion(factor1, factor2):
+    sumando1 = factor1 * factor2
+    return sumando1
 
-principal()
+def suma(sumando2):
+    return sumando2
+
+if  __name__=="__main__":
+    print("\n----- RESOLVIENDO OPERACIÓN MATEMÁTICA ------\n")
+    numero1 = float(input("Ingrese primer número: "))
+    numero2 = float(input("Ingrese segundo número: "))
+    numero3 = float(input("Ingrese tercer número: "))
+
+    resultado = multiplicacion(numero1, numero2) + suma(numero3)
+    print("El resultado es: "f"{numero1} * {numero2} + {numero3} = {resultado}")
+
+
